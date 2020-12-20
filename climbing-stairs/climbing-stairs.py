96c84280dde497f028e60b1f4aa9617e37a0744d
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2020-12-20 16:58:47 -0500

    Time: 32 ms (42.94%), Space: 14 MB (82.56%) - LeetHub

--- climbing-stairs/climbing-stairs.py
+++ climbing-stairs/climbing-stairs.py
@@ -1,18 +1,18 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-​
-        dp = [1, 1]
-​
-        return self.helper(n, dp)
-​
-    def helper(self, n, dp) -> int:
-​
+        my_dict = {}
+        return self.helper(n , my_dict)
+        
+        
+    def helper(self, n, my_dict):
+        
         if n <= 1:
             return 1
-        else:
-            if len(dp) > n:
-                return dp[n]
-            else:
-                dp.append(self.helper(n-1, dp) + self.helper(n-2, dp))
-                return dp[n]
         
+        else: 
+            if n in my_dict:
+                return my_dict[n]
+            else:
+                my_dict[n] = self.helper(n - 1, my_dict) + self.helper(n - 2, my_dict)
+            return my_dict[n]
+            
