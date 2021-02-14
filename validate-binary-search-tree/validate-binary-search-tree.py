fef1f5f13045db779289f5acf52a1f4439bb060e
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-02-13 19:52:29 -0500

    Time: 44 ms (74.68%), Space: 16.5 MB (56.77%) - LeetHub

--- validate-binary-search-tree/validate-binary-search-tree.py
+++ validate-binary-search-tree/validate-binary-search-tree.py
@@ -1,28 +1,23 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def isValidBST(self, root: TreeNode) -> bool:
-        
-        return self.helper(None,None,root)
-        
-        
-    def helper(self, lower_limit: int, upper_limit: int, root: TreeNode) -> bool:
-        low: bool = True
-        upper: bool = True
-            
-        if lower_limit != None and lower_limit >= root.val:
-            return False
-        if upper_limit != None and upper_limit <= root.val:
-            return False
-        
-        if root.left:
-            low = self.helper(lower_limit, (root.val), root.left)
-            
-        if root.right:
-            upper = self.helper(root.val, upper_limit, root.right)
-            
-        return low and upper
+    def isValidBST(self, root: TreeNode) -> bool:
+        
+        if not root:
+            return True
+    
+        return self.helper(root, float('-inf'), float('inf'))
+
+    def helper(self, root, min_val, max_val):
+        
+        if not root:
+            return True
+        
+        if root.val >= max_val or root.val <= min_val:
+            return False
+
+        return self.helper(root.left, min_val, min(root.val, max_val)) and self.helper(root.right, max(root.val, min_val), max_val)
