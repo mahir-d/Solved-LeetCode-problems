75d9e97045e08f36cda60b7d773c380346865901
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-02-14 21:15:06 -0500

    Time: 508 ms (68.95%), Space: 49.1 MB (74.66%) - LeetHub

--- minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -1,31 +1,36 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def minDepth(self, root: TreeNode) -> int:
-        
-        return self.helper(root)
-    
-    def helper(self, root: TreeNode)->int:
-        
-        if root == None:
-            return 0
-        
-        l = 0
-        r = 0
-        
-        if root.left:
-            l += self.helper(root.left)
-        if root.right:
-            r += self.helper(root.right) 
-        
-        if l == 0:
-            return r + 1
-        elif r == 0:
-            return l + 1
-        else:
-            return min(l, r) + 1
-            
+    def minDepth(self, root: TreeNode) -> int:
+        
+        
+        if not root:
+            return 0
+        
+        queue = [root]
+        level = 1
+        while queue:
+            
+            size = len(queue)
+            
+            curr_list = []
+            for i in range(size):
+            
+                curr_root= queue.pop(0)
+                
+                if not curr_root.left and not curr_root.right:
+                    return level
+                else:
+                    if curr_root.left:
+                        queue.append(curr_root.left)
+                    if curr_root.right:
+                        queue.append(curr_root.right)
+                        
+            level+=1
+        return level
+                
+                
