93a8949a8e2b40d993491e5c2d9149a47a990008
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-02-17 16:26:00 -0500

    Time: 52 ms (58.11%), Space: 15.4 MB (21.11%) - LeetHub

--- move-zeroes/move-zeroes.py
+++ move-zeroes/move-zeroes.py
@@ -1,30 +1,32 @@
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        
-        
-        zero_idx: int = 0
-        nums_len: int = len(nums)
-        
-        while zero_idx < nums_len:
-            
-            while zero_idx < nums_len and nums[zero_idx] != 0:
-                zero_idx += 1
-            
-            
-            
-            non_zero = zero_idx + 1
-            
-            while non_zero < nums_len and nums[non_zero] == 0:
-                non_zero += 1
-                
-             
-            if non_zero < nums_len and non_zero > zero_idx:
-                
-                nums[zero_idx] = nums[non_zero]
-                nums[non_zero] = 0
-                
-            zero_idx += 1
-            
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zero_p = 0
+        
+        non_z = 0
+        
+        
+        while non_z < len(nums) and zero_p < len(nums):
+            while zero_p < len(nums) and nums[zero_p] != 0:
+                zero_p +=1
+            
+            #z = 1
+
+            while non_z < len(nums) and nums[non_z] == 0:
+                non_z+=1
+            # non = 0
+            
+
+            if zero_p < len(nums) and non_z < len(nums)  and zero_p < non_z:
+                nums[zero_p] = nums[non_z]
+                nums[non_z] = 0
+                zero_p+=1
+                non_z+=1
+                
+            else:
+                if non_z < len(nums):
+                    non_z+=1
+                
+                
