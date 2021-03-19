805d5a8fe34245c3ff3ea02ea6102e5c16ddf03c
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-03-18 20:51:39 -0400

    Time: 32 ms (64.08%), Space: 14.1 MB (88.07%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,6 +1,5 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        
         KEYBOARD = {
             '2': 'abc',
             '3': 'def',
@@ -9,32 +8,37 @@ class Solution:
             '6': 'mno',
             '7': 'pqrs',
             '8': 'tuv',
-            '9': 'wxyz',
+            '9': 'wxyz'
         }
- 
-        
+        curr_d = list(digits)
+        visited = set()
+        len_d = len(digits)
         result = []
-        s_len = len(digits)
-        if s_len == 0:
-            return []
-        def helper(start, curr_list):
-            
-            
-            if len(curr_list) == s_len:
-                result.append("".join(curr_list))
+        def dfs(start, curr_d, curr_list):
+
+            if len(curr_list) == len_d:
+                if curr_list:
+                    result.append("".join(curr_list))
                 return
-            
-            
-            for i in KEYBOARD[digits[start]]:
-                
-                curr_list.append(i)
-                helper(start+1, curr_list)
-                curr_list.pop()
-                
-                
-            return
-        
-        helper(0, [])
+
+            for d in range(start, len(curr_d)):
+                curr_alpha = KEYBOARD[curr_d[d]]
+
+                for alpha in curr_alpha:
+                    curr_list.append(alpha)
+                    dfs(d+1, curr_d, curr_list)
+                    curr_list.pop()
+
+        dfs(0, curr_d, [])
         return result
-        
-        
+                    
+                
+                
+                
+                
+                
+                
+                
+                
+                
+            
