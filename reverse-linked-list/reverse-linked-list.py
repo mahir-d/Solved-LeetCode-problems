9a985035fc0295b15ddbd9da8c6e7c3316b7c765
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-04-23 16:59:32 -0400

    Time: 36 ms (62.18%), Space: 15.7 MB (43.99%) - LeetHub

--- reverse-linked-list/reverse-linked-list.py
+++ reverse-linked-list/reverse-linked-list.py
@@ -1,21 +1,21 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        
-        prevHead: "ListNode" = None
-        currHead: "ListNode" = head
-        
-        while currHead:
-            temp: "ListNode" = currHead.next
-            
-            currHead.next = prevHead
-            prevHead = currHead 
-            currHead = temp
-            
-        return prevHead
-        
-        
+    
+    def reverseList(self, head: ListNode) -> ListNode:
+            
+        pointer = head
+        prev = None
+        while pointer:
+            
+            temp = pointer.next
+            pointer.next = prev
+            
+            prev = pointer
+            pointer = temp
+            
+        return prev
+             
