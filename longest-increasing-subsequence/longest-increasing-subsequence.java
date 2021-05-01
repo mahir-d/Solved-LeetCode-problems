9926baf2b98dc894c7fbd680b03dfee88ab932a5
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-05-01 19:11:03 -0400

    Time: 63 ms (25.25%), Space: 38.4 MB (70.21%) - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.java
+++ longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -3,20 +3,17 @@ class Solution {
         
         int[] dp = new int[nums.length];
         Arrays.fill(dp, 1);
+        
         int max_len = 1;
-        for(int i = 0; i < nums.length; i++){
-            
+        for(int i = 0 ; i < nums.length; i++){
             for(int j = 0; j < i; j++){
-                
-                if(nums[j] >= nums[i]) continue;
-                
+                if(nums[j]>=nums[i])continue;
                 
                 dp[i] = Math.max(dp[j] + 1, dp[i]);
-                max_len = Math.max(dp[i],max_len);
+                max_len = Math.max(max_len, dp[i]);
             }
         }
         
         return max_len;
-        
     }
 }
