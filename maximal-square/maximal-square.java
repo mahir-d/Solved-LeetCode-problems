2d869cd2660bf08395ce30732e9acb0697a754c5
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-05-01 19:04:42 -0400

    Time: 3 ms (98.15%), Space: 42.3 MB (42.67%) - LeetHub

--- maximal-square/maximal-square.java
+++ maximal-square/maximal-square.java
@@ -2,41 +2,30 @@ class Solution {
     public int maximalSquare(char[][] matrix) {
         
         
-        int m = matrix.length;
+        int m = matrix.length; 
         int n = matrix[0].length;
         
         int[][] grid = new int[m][n];
-        int best = 0;
+        int max_s = 0;
         for(int i = 0; i < m; i++){
-            grid[i][0] = Character.getNumericValue(matrix[i][0]);
-            best = Math.max(grid[i][0],best);
-        }
-        for(int i = 0; i < n; i++){
-            grid[0][i] = Character.getNumericValue(matrix[0][i]);
-            best = Math.max(grid[0][i],best);
-        }
-        
-        
-        for(int[] arr:grid){
-            for (int i : arr){
-                System.out.print(i + " ");
+            for(int j = 0; j < n; j++){
+                grid[i][j] = matrix[i][j] - '0';
+                max_s = Math.max(grid[i][j], max_s);
             }
-            System.out.println();
         }
+        
+        
         
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
-                if(matrix[i][j] == '0'){
-                    continue;   
-                }
+                if(grid[i][j] == 0)continue;
                 
                 grid[i][j] = Math.min(Math.min(grid[i-1][j], grid[i][j-1]), grid[i-1][j-1]) + 1;
-                best = Math.max(best, grid[i][j]);
-               
+                max_s = Math.max(grid[i][j], max_s);
             }
         }
         
-        return best* best;
+        return max_s * max_s;
         
         
         
