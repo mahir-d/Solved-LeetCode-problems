81e1bed7206920c957e86e8b896eb9721db0e527
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-05-01 17:47:10 -0400

    Time: 0 ms (100.00%), Space: 36.4 MB (46.19%) - LeetHub

--- house-robber/house-robber.java
+++ house-robber/house-robber.java
@@ -1,22 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
         
+        if (nums.length == 0){
+            return 0;
+        }
         if (nums.length == 1){
             return nums[0];
-        }
-        if(nums.length <= 2){
-            return Math.max(nums[0], nums[1]);
         }
         
         nums[1] = Math.max(nums[0], nums[1]);
         
+        int max_i = nums[1];
         for(int i = 2; i < nums.length; i++){
-            nums[i] = Math.max(nums[i-1], nums[i] + nums[i-2]);
+            nums[i] = Math.max(nums[i] + nums[i-2], nums[i-1]);
+            max_i = Math.max(nums[i], max_i);
         }
         
-        
-        return nums[nums.length - 1];
-        
+        return max_i;
         
     }
 }
