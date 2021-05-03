2b494ecae87f00b755b4eaeb5dc9a296dc045883
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-05-03 18:51:58 -0400

    Time: 2 ms (100.00%), Space: 36.2 MB (84.83%) - LeetHub

--- coin-change-2/coin-change-2.java
+++ coin-change-2/coin-change-2.java
@@ -1,19 +1,20 @@
 class Solution {
     public int change(int amount, int[] coins) {
         
-        
         int[] dp = new int[amount+1];
         dp[0] = 1;
         
-        for (int denom : coins){
+        for(int denom : coins){
             
-            for(int i = denom; i < amount + 1; i++){
+            
+            for(int i = denom; i < amount+ 1; i++){
+                
                 int remain = i - denom;
                 dp[i] = dp[remain] + dp[i];
             }
         }
         
-        
         return dp[amount];
+        
     }
 }
