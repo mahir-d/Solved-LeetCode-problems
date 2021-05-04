8213135e433c32264f8985111a72400d8a2f40ee
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-05-03 20:22:37 -0400

    Time: 40 ms (55.81%), Space: 14.7 MB (13.79%) - LeetHub

--- word-break/word-break.py
+++ word-break/word-break.py
@@ -1,22 +1,35 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
-        my_dict = {}
+        memo = {}
         
-        def helper(i):
-            if i in my_dict:
-                return my_dict[i]
-            if i >= len(s):
+        def dfs(start):
+        
+            if start == len(s):
+                return True
+            
+            if start in memo:
+                return memo[start]
+            
+            for i in range(start, len(s)):
+                
+                if s[start:i+1] not in wordDict:
+                    continue
+                if dfs(i+1):
+                    memo[start] = True
                     return True
-            for word in wordDict:
-                if s[i:].startswith(word):
-                    if helper(i + len(word)):
-                        my_dict[i] = True
-                        return True
-            my_dict[i] = False
+                
+            
+            memo[start] = False
             return False
+        
+        
+        return dfs(0)
+            
+            
+            
             
         
-        return helper(0)
+        
         
         
