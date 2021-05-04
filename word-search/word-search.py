c30cab91aca69a0713501b4dfaf2569e08c2c3d0
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-05-04 19:55:27 -0400

    Time: 3600 ms (72.85%), Space: 13.9 MB (99.84%) - LeetHub

--- word-search/word-search.py
+++ word-search/word-search.py
@@ -1,40 +1,30 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         
+        x = len(board)
+        y = len(board[0])
         
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                
+        def dfs(i,j,index):
+            if index == len(word):
+                return True
+            if i<0 or i>= len(board) or j<0 or j>=len(board[0]) or board[i][j] != word[index]:
+                return False
+            temp = board[i][j]
+            board[i][j] = "*"
+            
+            x = dfs(i-1,j,index+1) or dfs(i+1,j,index+1) or dfs(i,j+1,index+1) or dfs(i,j-1,index+1)
+            
+            board[i][j] = temp
+            return x
+        for i in range(x):
+            for j in range(y):
                 if board[i][j] == word[0]:
-                    if self.helper(i,j,board,word, 0):
+                    if dfs(i,j,0):
                         return True
-        
         return False
-    def helper(self, row, col, board, word, i):
-        
-        if i == len(word):
-            return True
-        
-        if row < 0 or row >= len(board) or col >= len(board[0]) or col < 0:
-            return False
-        
-        if word[i] != board[row][col]:
-            return False
-        
-        flag = False
-        temp = board[row][col]
-        board[row][col] = "*"
-        
-        flag = self.helper(row+1,col,board,word,i+1) or self.helper(row-1, col, board,word,i+1) or self.helper(row,col+1, board,word,i+1) or self.helper(row, col-1, board, word, i+1)
-       
-        board[row][col] = temp
-        return flag
-        
-        
-        
-        
-    
-            
-                
-            
-            
+
+
+
+
+
+
