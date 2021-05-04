f6bac70805c802f5f1549cbc463928e238671029
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-05-04 15:22:15 -0400

    Time: 692 ms (24.93%), Space: 30.5 MB (31.46%) - LeetHub

--- palindrome-partitioning/palindrome-partitioning.py
+++ palindrome-partitioning/palindrome-partitioning.py
@@ -1,28 +1,57 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         
-        
-        
         result = []
         
-        def helper(i ,combo):
+        def dfs(start, currList):
             
-            if i == len(s):
-                result.append(combo[:])
+            if start >= len(s):
+                result.append(currList[:])
+                return
+            
+            for i in range(start, len(s)):
+                curr_s = s[start: i+1]   
+                if self.validPalindrome(curr_s):
+                    currList.append(curr_s)
+                    dfs(i+1, currList)
+                    currList.pop()
+        dfs(0, [])
+        return result
                 
-            for j in range(i, len(s)):
+    def validPalindrome(self, s: str):
+
+        mid = len(s)//2
+
+        if len(s) % 2 == 0:
+
+            left = mid-1
+            right = mid
+
+            while left >= 0 and right < len(s):
+
+                if s[left] != s[right]:
+                    return False
+
+                else:
+                    left-=1
+                    right+=1
+            return True
+        else:
+
+            left = mid-1
+            right = mid+1
+
+            while left >= 0 and right < len(s):
+
+                if s[left] != s[right]:
+                    return False
+
+                else:
+                    left-=1
+                    right+=1
+            return True
+
+
                 
-                temp = s[i:j+1]
-                
-                if temp == temp[::-1]:
-                    combo.append(temp)
-                    helper(j+1, combo)
-                    combo.pop()
-            return
                     
                 
-                
-            
-            
-        helper(0, [])
-        return result
