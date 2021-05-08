c9be3db9f004af26a95f8327ee0afbe82206cce6
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-05-08 18:46:04 -0400

    Time: 152 ms (67.66%), Space: 14.3 MB (84.70%) - LeetHub

--- coin-change-2/coin-change-2.py
+++ coin-change-2/coin-change-2.py
@@ -1,19 +1,18 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         
-        dp = [0 for _ in range(amount + 1)]
-        dp[0] = 1
         
+        
+        dp = [0] * (amount+1)
+        
+        dp[0] = 1
         
         for denom in coins:
             
             for i in range(denom, amount+1):
+
                 
                 remain = i - denom
-                dp[i] = dp[i] + dp[remain]
+                dp[i] = dp[remain] + dp[i]
                 
-        return dp[-1]
-            
-        
-        
-        
+        return dp[amount]
