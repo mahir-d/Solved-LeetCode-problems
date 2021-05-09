229a5c6d1203365ae163ebafd5a419b8d5daa949
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-05-09 18:02:34 -0400

    Time: 32 ms (64.36%), Space: 14.6 MB (11.63%) - LeetHub

--- decode-ways/decode-ways.py
+++ decode-ways/decode-ways.py
@@ -2,8 +2,10 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         
         memo = {}
-
         def dfs(idx):
+            
+            if idx in memo:
+                return memo[idx]
             
             if idx >= len(s):
                 return 1
@@ -11,18 +13,23 @@ class Solution:
             if s[idx] == "0":
                 return 0
             
-            if idx == len(s) - 1:
+            if idx == len(s) -1:
                 return 1
             
-            if idx in memo:
-                return memo[idx]
-            ans = 0
-            ans = dfs(idx + 1)
+            count = 0
+            count = dfs(idx+1)
             
             if 10 <= int(s[idx: idx+2]) <= 26:
-                ans += dfs(idx+2)
-                
-            memo[idx] = ans
-            return ans
+                count += dfs(idx+2)
+            
+            memo[idx] = count
+            return count
         
         return dfs(0)
+    
+    
+        
+            
+            
+            
+        
