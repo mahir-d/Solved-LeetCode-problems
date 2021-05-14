ea4a153ff2fd073a5a81271a3fc7d390625c739e
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-05-14 13:08:30 -0400

    Time: 32 ms (57.91%), Space: 14.3 MB (18.35%) - LeetHub

--- house-robber/house-robber.py
+++ house-robber/house-robber.py
@@ -1,19 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        if not nums:
-            return 0
-        
         if len(nums) <= 2:
             return max(nums)
         
-        nums[1] = max(nums[0], nums[1])
+        
+        dp = [float('inf')] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
         
         
         for i in range(2, len(nums)):
-
-            nums[i] = max(nums[i] + nums[i-2], nums[i-1])
             
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
             
-            
-        return nums[-1]
+        
+        return dp[-1] if dp[-1] != float('inf') else -1
+        
