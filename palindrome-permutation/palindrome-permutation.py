dc86d7a6f8f7baa47f0e341429fb2f59037532d6
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-05-20 12:44:53 -0400

    Time: 24 ms (93.06%), Space: 14.3 MB (43.81%) - LeetHub

--- palindrome-permutation/palindrome-permutation.py
+++ palindrome-permutation/palindrome-permutation.py
@@ -1,11 +1,24 @@
 class Solution:
-    def canPermutePalindrome(self, s: str) -> bool:
-        my_set = set()
-        
-        for ch in s:
-            if ch in my_set:
-                my_set.remove(ch)
-            else:
-                my_set.add(ch)
-                
-        return len(my_set) <= 1
+    def canPermutePalindrome(self, s: str) -> bool:
+        
+        c_s = Counter(s)
+        
+        l_s = len(s)
+        
+        count_o = 0
+        for k,v in c_s.items():
+            if v % 2 != 0:
+                count_o+=1
+                
+                
+        if l_s % 2 != 0:
+            return count_o <= 1
+        else:
+            return count_o == 0
+        
+            
+            
+        
+        
+        
+        
