f9d7282d330aee11be9c1f516dd0ac73b3b6f300
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-05-23 17:01:28 -0400

    Time: 32 ms (54.20%), Space: 14.5 MB (26.87%) - LeetHub

--- spiral-matrix/spiral-matrix.py
+++ spiral-matrix/spiral-matrix.py
@@ -1,34 +1,38 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         
-        
+        direR = [0,1,0,-1]
+        direC = [1,0,-1,0]
         visited = set()
+        directions = [0,1,2,3]
         result = []
         m = len(matrix)
         n = len(matrix[0])
-        dir_r = [0,1,0,-1]
-        dir_c = [1,0,-1,0]
         
-        
-        def dfs(i, j,direc):
-            print(i, " ", j)
-            if i < 0 or i >= m or j < 0 or j>= n or (i, j) in visited:
-                return
+        def backtrack(i, j, d):
+            
+            print(result)
+            
+            if i < 0 or i >= m or j < 0 or j >= n or (i,j) in visited:
+                return 
             
             result.append(matrix[i][j])
             visited.add((i,j))
-            new_i = i+dir_r[direc]
-            new_j = j+dir_c[direc]
+            
+            new_i = direR[d] + i
+            new_j = direC[d] + j
+            
+            
+            
             if new_i < 0 or new_i >= m or new_j < 0 or new_j >= n or (new_i,new_j) in visited:
-                new_dir = (direc+1)%4
-                dfs(i+dir_r[new_dir],j+dir_c[new_dir],new_dir)
-                return
-            
-            dfs(new_i,new_j,direc)
-            return
-            
+                d = (d+1) % 4
+                backtrack(i + direR[d],j + direC[d],d)
+                return 
+            else:
+                backtrack(new_i, new_j, d)
         
-        dfs(0,0,0)
+        
+        backtrack(0,0,0)
         return result
-            
         
+                
