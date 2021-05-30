aa2371f95c9637bb672cb289b3987d68b2776eb4
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-05-30 18:27:33 -0400

    Time: 60 ms (33.32%), Space: 14.8 MB (63.32%) - LeetHub

--- trapping-rain-water/trapping-rain-water.py
+++ trapping-rain-water/trapping-rain-water.py
@@ -1,33 +1,33 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         
+        if not height:
+            return 0
         
-        max_left = height[:]
-        max_right = height[:]
+        max_left = [0] * len(height)
+        max_right = [0] * len(height)
         
-        curr_max = 0
-        for i in range(0, len(max_left)):
-            
-            max_left[i] = curr_max
-            curr_max = max(curr_max, height[i])
-            
-        curr_max = 0
-        for i in range(len(height)-1, -1,-1):
-            max_right[i] = curr_max
-            curr_max = max(curr_max, height[i])
-            
+        max_height = height[0]
         
-        curr_volume = 0
+        for i in range(1, len(height)):
+            
+            max_left[i] = max_height
+            max_height = max(max_height, height[i])
+            
+        max_height = height[-1]
         
-        for i in range(len(height)):
-            min_h = min(max_left[i], max_right[i])
-            if min_h > height[i]:
-                curr_volume += min_h - height[i]
+        for i in range(len(height)-2,-1, -1):
+            max_right[i] = max_height
+            max_height = max(max_height, height[i])
             
-        return curr_volume
+        # print(max_left)
+        # print(max_right)
+        
+        
+        total = 0
+        for i in range(1, len(height)-1):
+            if min(max_left[i], max_right[i]) > height[i]:
+                total += (min(max_left[i], max_right[i])  - height[i])
             
             
-            
-            
-            
-            
+        return total
