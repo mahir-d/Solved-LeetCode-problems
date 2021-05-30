9604d95c9521b4d3e59f2b62e34cd4670917b7a3
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-05-30 19:00:20 -0400

    Time: 32 ms (90.29%), Space: 14.1 MB (95.10%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.py
+++ merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,37 +1,39 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        
-        if l1 == None:
-            return l2
-        elif l2 == None:
-            return l1
-        
-        dummy: ListNode = ListNode(0, l1)
-            
-        pointer = dummy
-        
-        while pointer.next != None:
-            
-            if l2 != None:
-                while pointer.next != None and l2.val > pointer.next.val:
-                    pointer = pointer.next
-                
-                temp1 = pointer.next    
-                temp = l2.next
-                pointer.next = l2
-                l2.next = temp1
-                l2 = temp
-                
-            else:
-                break
-                
-                
-        return dummy.next
-                
-        
-        
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        if not l1 and not l2:
+            return None
+        
+        if not l1 or not l2:
+            return l1 or l2
+        
+        senital = ListNode(0, l1)
+        prev = senital
+        dummy = l1
+        
+        while l2:
+            
+            while dummy and dummy.val <= l2.val:
+                prev = dummy
+                dummy = dummy.next 
+            
+            if not dummy:
+                prev.next = l2
+                break
+            else:
+                temp = l2.next
+                prev.next = l2
+                l2.next = None
+                l2.next = dummy
+                dummy = l2
+                l2 = temp
+                
+        return senital.next
+                
+                
+        
