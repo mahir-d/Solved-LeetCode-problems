589f93c188ed63ca3c5553055213d5450ef92663
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-05-30 17:50:32 -0400

    Time: 44 ms (30.63%), Space: 14.5 MB (23.31%) - LeetHub

--- word-break/word-break.py
+++ word-break/word-break.py
@@ -1,25 +1,20 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         memo = {}
-        
-        def dfs(start):
+        def backtrack(start):
             
-            if start >= len(s):
+            if start == len(s):
                 return True
-            
             if start in memo:
                 return memo[start]
-            
             for i in range(start, len(s)):
-                
-                if s[start: i+1] not in wordDict:
+                if s[start:i+1] not in wordDict:
                     continue
-                res = dfs(i+1)
+                res = backtrack(i+1)
                 if res:
                     memo[start] = True
                     return True
-            
             memo[start] = False
             return False
+        return backtrack(0)
         
-        return dfs(0)
