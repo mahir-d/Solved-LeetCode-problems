17d9f44915b0b81b6e40e04f7fcfecd831cc1738
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-05-30 17:33:03 -0400

    Time: 692 ms (53.26%), Space: 15.6 MB (13.46%) - LeetHub

--- shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -3,52 +3,38 @@ class Solution:
         
         if grid[0][0] == 1 or grid[-1][-1] == 1:
             return -1
-    
         
-        dire_r = [0,0,-1,1,-1,-1,1,1]
-        dire_c = [1,-1,0,0,-1,1,1,-1]
-        
-        m = len(grid)
+        m =len(grid)
         n = len(grid[0])
+        
+        direc = [(0,1),(0,-1),(-1,0),(1,0),(1,1),(-1,-1),(-1,1),(1,-1)]
+        
         
         queue = [(0,0)]
         
-        level = 1
-        
+        visited = set()
+        level = 1        
         while queue:
-                
+            
             len_q = len(queue)
             
             for _ in range(len_q):
+                
                 curr_i, curr_j = queue.pop(0)
                 
                 if curr_i == m-1 and curr_j == n-1:
                     return level
                 
-                for d in range(len(dire_r)):
-                    new_i = dire_r[d] + curr_i
-                    new_j = dire_c[d] + curr_j
-        
-                    if new_i < 0 or new_i >= m or new_j < 0 or new_j >=n or grid[new_i][new_j] != 0:
+                for d in range(8):
+                    new_i = curr_i + direc[d][0]
+                    new_j = curr_j + direc[d][1]
+                    
+                    if new_i < 0 or new_i >= m or new_j < 0 or new_j >= n or (new_i, new_j) in visited or grid[new_i][new_j]!=0:
                         continue
-                    grid[new_i][new_j] = 2                
+                    visited.add((new_i, new_j))
                     queue.append((new_i, new_j))
-            
+                    
             level += 1
-                    
+        
         return -1
-                    
-                    
-                    
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         
