6e9d65561ef7838e26ce44aba3952a0d37e1d573
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-06-02 12:40:12 -0400

    Time: 80 ms (5.99%), Space: 14.4 MB (35.26%) - LeetHub

--- rotting-oranges/rotting-oranges.py
+++ rotting-oranges/rotting-oranges.py
@@ -4,45 +4,50 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
         
-        
-        
-        direc = [(1,0), (-1,0), (0,1), (0,-1)]
-        queue = []
         fresh = 0
+        queue = []
+        
+        
         for i in range(m):
             for j in range(n):
-            
+                
                 if grid[i][j] == 2:
                     queue.append((i,j))
-                elif grid[i][j] == 1:
-                    fresh += 1
-        
+                if grid[i][j] == 1:
+                    fresh+=1
+                    
+        print(fresh, queue)
         if fresh == 0:
             return 0
+        # if len(queue) == 0:
+        #     return 0
         
         level = -1
-        
-        
+        direc = [(0,1),(0,-1),(-1,0),(1,0)]
         while queue:
-            # print(queue)
-            # print(level)
-            len_q = len(queue)
             
+            len_q = len(queue)
             for _ in range(len_q):
                 
-                i,j = queue.pop(0)
-                for d in range(len(direc)):
-                    new_i = i + direc[d][0]
-                    new_j = j + direc[d][1]
+                curr_i, curr_j = queue.pop(0)
+                
+                
+                for d in range(4):
+                    new_i = curr_i + direc[d][0]
+                    new_j = curr_j + direc[d][1]
                     
                     if new_i < 0 or new_i >= m or new_j < 0 or new_j >= n or grid[new_i][new_j] != 1:
                         continue
-                    grid[new_i][new_j] = 2
-                    fresh -= 1
+                    
+                    
+                    grid[new_i][new_j] = 2  
+                    fresh-=1
                     queue.append((new_i, new_j))
-                        
+                
+                
             level += 1
-            
-            
+        
         return level if fresh == 0 else -1
-            
+        
+    
+        
