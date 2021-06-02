bbaa7ca15ec1f8a48fa8352c804e9b24c9938dea
/mahir Dhall <34060904+mahir-d@users.noreply.github.com>
2021-06-02 11:44:04 -0400

    Time: 204 ms (6.05%), Space: 15.3 MB (84.19%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,30 +1,31 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        def dfs(i, j):
-            m = len(grid)
-            n = len(grid[0])    
-            
-            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] != "1":
-                return
+        m = len(grid)
+        n = len(grid[0])
+        direc = [(0,1),(0,-1), (-1,0),(1,0)]
+        
+        
+        
+        def dfs(i,j):
             
             grid[i][j] = "0"
             
-            dfs(i+1, j)
-            dfs(i-1, j)
-            dfs(i, j+1)
-            dfs(i, j-1)
-            return 
+            for d in range(4):
+                
+                new_i = i + direc[d][0]
+                new_j = j + direc[d][1]
+                
+                if new_i < 0 or new_i >= m or new_j < 0 or new_j >= n or grid[new_i][new_j] == "0":
+                    continue
+                dfs(new_i, new_j)
+                
             
-        count = 0 
-        m = len(grid)
-        n = len(grid[0])  
+        num_islands = 0    
         for i in range(m):
             for j in range(n):
-                
-                if grid[i][j] == "1":
-                    dfs(i, j)
-                    count+=1
-        return count
-        
-        
+                if grid[i][j] != "0":
+                    dfs(i,j)
+                    num_islands += 1
+        return num_islands
+    
